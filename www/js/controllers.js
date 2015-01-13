be80f5c6854befd0294265a376d8f667fcd3060c
furrmao <mikefurrhotmail.co.uk>
2015-01-13 09:41:18 +0000

    added workstation power up/down logic and general tidy up.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,118 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('BootstrapCtrl',
+    ['$scope', 'CacheService', '$state',
+        function ($scope, CacheService, $state) {
+            $scope.name = 'bootstrap';
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+            var cachedView = JSON.parse(CacheService.localStorage.read('view'));
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+            if (cachedView) {
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+                $state.go(cachedView.stateId);
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+            }
+            else{
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+                $state.go("app.home");
+
+            }
+
+        }])
+    .controller('AppCtrl',
+    ['$scope', '$timeout', '$ionicLoading', '$ionicHistory', 'CacheService',
+        function ($scope, $timeout, $ionicLoading, $ionicHistory, CacheService) {
+
+            // cache the current view.
+
+            var view = $ionicHistory.currentView();
+            var cachedView = JSON.parse(CacheService.localStorage.read('view'));
+
+
+            if (cachedView) {
+
+                if (view != cachedView) {
+
+                    CacheService.localStorage.write('view', JSON.stringify(view));
+
+                }
+
+            }
+            else {
+
+                CacheService.localStorage.write('view', JSON.stringify(view));
+
+            }
+
+
+            console.log($ionicHistory.currentView()); // todo: delete me
+
+            $scope.isOn = true;
+
+            $scope.turnOff = function () {
+
+                $ionicLoading.show({
+
+                    "template": "Powering down workstation.  Please wait..."
+
+                });
+
+                $timeout(function () {
+
+                    $ionicLoading.hide();
+
+                    $scope.isOn = false;
+
+                }, 3000);
+
+            };
+
+            $scope.turnOn = function () {
+
+                $ionicLoading.show({
+
+                    "template": "Powering on workstation.  Please wait..."
+
+                });
+
+                $timeout(function () {
+
+                    $ionicLoading.hide();
+
+                    $scope.isOn = true;
+
+                }, 3000);
+
+            };
+
+        }])
+
+;
+
+
+//.controller('DashCtrl', function($scope) {})
+//
+//.controller('ChatsCtrl', function($scope, Chats) {
+//  $scope.chats = Chats.all();
+//  $scope.remove = function(chat) {
+//    Chats.remove(chat);
+//  }
+//})
+//
+//.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//  $scope.chat = Chats.get($stateParams.chatId);
+//})
+//
+//.controller('FriendsCtrl', function($scope, Friends) {
+//  $scope.friends = Friends.all();
+//})
+//
+//.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//  $scope.friend = Friends.get($stateParams.friendId);
+//})
+//
+//.controller('AccountCtrl', function($scope) {
+//  $scope.settings = {
+//    enableFriends: true
+//  };
+//});
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,65 @@
-angular.module('starter.services', [])
+angular.module('app.services', [])
+
+    .factory('CacheService', [function () {
+        return{
+            "name": 'CacheService',
+            "localStorage": {
+                "write": writeLocal,
+                "read": readLocal,
+                "update": updateLocal,
+                "delete": deleteLocal
+            }
+        }
+
+        // localStorage operations
+        function writeLocal(key, val) {
+
+            try {
+
+                localStorage.setItem(key, val);
+
+            }
+            catch (ex) {
+
+                throw ex;
+
+            }
+
+        }
+
+        function updateLocal(key, val){
+
+            return writeLocal(key, val);
+
+        }
+
+        function readLocal(key) {
+
+            try {
+
+                return localStorage.getItem(key);
+
+            }
+            catch (ex) {
+
+                throw ex;
+
+            }
+        }
+
+        function deleteLocal(key) {
+
+            try {
+
+                localStorage.removeItem(key);
+
+            } catch (ex) {
+
+            }
+
+        }
+
+    }])
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
